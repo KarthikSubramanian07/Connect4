2ff5fb53f41577b062580c9644484792e8a289ce
JackHtoo <108778457+JackHtoo@users.noreply.github.com>
2023-10-23 19:33:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,55 +14,93 @@ rowCount = 6
 columnCount = 7
 
 # Create an empty game board
+
+
 def create_board():
     board = np.zeros((rowCount, columnCount))
     return board
 
+
 # Place a piece on the board
+
+
 def drop_piece(board, row, col, piece):
     board[row][col] = piece
 
+
 # Check if a column is a valid location to place a piece
+
+
 def is_valid_location(board, col):
     return board[rowCount - 1][col] == 0
 
+
 # Find the next available row in a column
+
+
 def get_next_open_row(board, col):
     for r in range(rowCount):
         if board[r][col] == 0:
             return r
 
+
 # Display the game board
+
+
 def print_board(board):
     print(np.flip(board, 0))
 
+
 # Check if a player has won
+
+
 def winning_move(board, piece):
     # Check horizontal locations for a winning patern
     for c in range(columnCount - 3):
         for r in range(columnCount):
-            if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][c + 3] == piece:
+            if (
+                board[r][c] == piece
+                and board[r][c + 1] == piece
+                and board[r][c + 2] == piece
+                and board[r][c + 3] == piece
+            ):
                 return True
 
     # Check vertical locations for a wining patern
     for c in range(columnCount):
         for r in range(rowCount - 3):
-            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][c] == piece:
+            if (
+                board[r][c] == piece
+                and board[r + 1][c] == piece
+                and board[r + 2][c] == piece
+                and board[r + 3][c] == piece
+            ):
                 return True
 
     # Check positively sloped diagonals
     for c in range(columnCount - 3):
         for r in range(rowCount - 3):
-            if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][c + 3] == piece:
+            if (
+                board[r][c] == piece
+                and board[r + 1][c + 1] == piece
+                and board[r + 2][c + 2] == piece
+                and board[r + 3][c + 3] == piece
+            ):
                 return True
 
     # Check negatively sloped diagonals
     for c in range(columnCount - 3):
         for r in range(3, rowCount):
-            if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][c + 3] == piece:
+            if (
+                board[r][c] == piece
+                and board[r - 1][c + 1] == piece
+                and board[r - 2][c + 2] == piece
+                and board[r - 3][c + 3] == piece
+            ):
                 return True
 
     return False
+
 
 # Initialize pygame
 pygame.init()
@@ -70,12 +108,12 @@ pygame.init()
 # Define square size
 squareSize = 100
 
-#Define screen dimensions
+# Define screen dimensions
 width = columnCount * squareSize
 height = (rowCount + 1) * squareSize
 size = (width, height)
 
-#Define screen radius
+# Define screen radius
 radius = int(squareSize / 2 - 5)
 
 # Create the game screen
@@ -92,7 +130,6 @@ game_over = False
 turn = 0
 
 while not game_over:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -126,11 +163,36 @@ while not game_over:
                 # Draw the updated board
                 for c in range(columnCount):
                     for r in range(rowCount):
-                        pygame.draw.rect(screen, blue, (c * squareSize, r * squareSize + squareSize, squareSize, squareSize))
+                        pygame.draw.rect(
+                            screen,
+                            blue,
+                            (
+                                c * squareSize,
+                                r * squareSize + squareSize,
+                                squareSize,
+                                squareSize,
+                            ),
+                        )
                         if board[r][c] == 1:
-                            pygame.draw.circle(screen, red, (int(c * squareSize + squareSize / 2), int(r * squareSize + squareSize + squareSize / 2)), radius)
+                            pygame.draw.circle(
+                                screen,
+                                red,
+                                (
+                                    int(c * squareSize + squareSize / 2),
+                                    int(r * squareSize + squareSize + squareSize / 2),
+                                ),
+                                radius,
+                            )
                         elif board[r][c] == 2:
-                            pygame.draw.circle(screen, yellow, (int(c * squareSize + squareSize / 2), int(r * squareSize + squareSize + squareSize / 2)), radius)
+                            pygame.draw.circle(
+                                screen,
+                                yellow,
+                                (
+                                    int(c * squareSize + squareSize / 2),
+                                    int(r * squareSize + squareSize + squareSize / 2),
+                                ),
+                                radius,
+                            )
                 pygame.display.update()
 
                 turn += 1
